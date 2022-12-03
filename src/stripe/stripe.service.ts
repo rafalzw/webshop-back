31512466a07ba1e3d816c1dd50c04904b31f3938
Rafal Zw <zwierz.rafal@gmail.com>
2022-12-03 16:51:25 +0100

    Implemented createCustomer method for Stripe.

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { config } from '../config/config';
 
 @Injectable()
-export class StripeService {}
+export class StripeService {
+  private stripe: Stripe;
+
+  constructor() {
+    this.stripe = new Stripe(config.stripeKey, {
+      apiVersion: '2022-11-15',
+    });
+  }
+  public async createCustomer(name: string, email: string) {
+    return this.stripe.customers.create({
+      name,
+      email,
+    });
+  }
+}
