141ca58f49fa0cf54b8a0b267a97ad6bf9d7948b
Rafal Zw <zwierz.rafal@gmail.com>
2022-12-03 16:09:15 +0100

    Enabled Cors.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { config } from './config/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: config.corsOrigin, credentials: true });
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   await app.listen(3001);
