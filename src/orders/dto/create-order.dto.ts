582c984461ff5670da95b2fbf18efa6aeafe6fba
Rafal Zw <zwierz.rafal@gmail.com>
2023-02-02 22:25:10 +0100

    Added validation to CreateOrderDto.

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -3,10 +3,22 @@ import {
   OrderInterface,
   OrderedProducts,
 } from '../../interfaces/order';
+import {
+  IsArray,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateOrderDto implements OrderInterface {
+  @IsObject()
   address: Address;
+  @IsNumber()
   amount: number;
+  @IsArray()
   products: OrderedProducts[];
+  @IsOptional()
+  @IsString()
   userId?: string;
 }
