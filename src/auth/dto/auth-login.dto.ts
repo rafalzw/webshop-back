762e59aed70856f05731eb1c6ac0a167a29036bd
Rafal Zw <zwierz.rafal@gmail.com>
2023-02-02 22:06:45 +0100

    Added validation to LoginDto.

--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -1,4 +1,23 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Length,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class AuthLoginDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(4, 20)
   username: string;
+
+  @MaxLength(255, {
+    message: 'Password is too long',
+  })
+  @MinLength(5, {
+    message: 'Password is too short. Min. 5 characters',
+  })
+  @IsNotEmpty()
   password: string;
 }
